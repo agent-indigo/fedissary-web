762c5802ccf749b567d85be12d2a733f57eddd2b
agent-indigo <bradenhynes@gmail.com>
2024-02-19 17:38:36 -0330

    Optional cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 const App = () => {
   return (
     <>
-
+      <h1>Hello World!</h1>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const index = (() => {
   const root = ReactDOM.createRoot(document.getElementById('root'))
   root.render(
     <React.StrictMode>
-      <App />
+      <App/>
     </React.StrictMode>
   )
 })()
